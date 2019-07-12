01afddc48c211011bafc4355e530a78d0652120e
cq99 <52830532+cq99@users.noreply.github.com>
2019-07-13 04:59:33 +0800

    Update and rename install/install_6.0.sh to 安装/install_6.0.sh

--- 安装/install_6.0.sh
+++ 安装/install_6.0.sh
@@ -7,10 +7,10 @@ is64bit=`getconf LONG_BIT`
 if [ -f "/usr/bin/apt-get" ];then
 	isDebian=`cat /etc/issue|grep Debian`
 	if [ "$isDebian" != "" ];then
-		wget -O install.sh https://raw.githubusercontent.com/cq99/bt-6.9.6-pro/master/install/install-ubuntu_6.0.sh && bash install.sh
+		wget -O install.sh https://raw.githubusercontent.com/cq99/bt-6.9.8-pro/master/install/install-ubuntu_6.0.sh && bash install.sh
 		exit;
 	else
-		wget -O install.sh https://raw.githubusercontent.com/cq99/bt-6.9.6-pro/master/install/install-ubuntu_6.0.sh && sudo bash install.sh
+		wget -O install.sh https://raw.githubusercontent.com/cq99/bt-6.9.8-pro/master/install/install-ubuntu_6.0.sh && sudo bash install.sh
 		exit;
 	fi
 fi
